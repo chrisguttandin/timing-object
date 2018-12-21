06adc51aa3f4408e52e5814be1781742f99d3225
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-12-21 01:33:58 +0100

    fix: remove redundant type definitions

--- src/factories/illegal-value-error.ts
+++ src/factories/illegal-value-error.ts
@@ -1,10 +1,5 @@
 import { TIllegalValueErrorFactory } from '../types';
 
-// @todo Remove this declaration again if TypeScript supports the DOMException constructor.
-declare const DOMException: {
-    new (message: string, name: string): DOMException;
-};
-
 export const createIllegalValueError: TIllegalValueErrorFactory = () => {
     try {
         return new DOMException('', 'IllegalValueError');
